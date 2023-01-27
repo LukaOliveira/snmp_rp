e6c349c86ac962003ab0ae3b704b774be6eb1667
Pedro Luka Oliveira <lukaoliveira1401@gmail.com>
2023-01-27 08:29:46 -0300

    Ajuste lib, pyproject e readme

--- src/lib.rs
+++ src/lib.rs
@@ -15,32 +15,27 @@ fn get(oid: &str, host: &str, port: &str, community: &str) -> PyResult<String>{
 
     let oid_converted = &parse_oid(oid);
 
-    let _sys_descr_oid = oid_converted;
+    let _sys_descr_oid  = oid_converted;
 	let community       = community.as_bytes();
 	let timeout         = Duration::from_secs(2);
   	      	
-	let ip = host.to_string();
+	let ip 		   = host.to_string();
 	let port: &str = port;
 
-	let c = String::from(ip + ":" + port);  
+	let c 	 = String::from(ip + ":" + port);  
 	let oid  = _sys_descr_oid.clone();
 		
 	let mut snmp_get = String::new();
-	
-	let mut sess = SyncSession::new(c, community, Some(timeout), 0).unwrap();
-	
-		
+	let mut sess 	 = SyncSession::new(c, community, Some(timeout), 0).unwrap();
+
 	let mut response = sess.get(&oid).unwrap();	
-	
+
 	for (_desc,val) in response.varbinds.next(){
 		snmp_get = (format!("{:?}", val)).to_string();
 		break;
 	}
 
-    Ok((snmp_get).to_string())
-
-
-    
+    Ok(snmp_get)
 
 }
 
